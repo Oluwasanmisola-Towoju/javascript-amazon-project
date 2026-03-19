e278a70bb8b9263876261cc4223eedd44015d0e0
Oluwasanmisola Towoju <oluwasanmisolatowoju@gmail.com>
2026-03-19 20:23:45 +0100

    Convert loadProducts from XHR to Fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,13 @@
 import {renderOrderSummary} from './checkout/order-summary.js';
 import {renderPaymentSummary} from './checkout/payment-summary.js';
-import {loadProducts} from '../data/products.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve();
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
