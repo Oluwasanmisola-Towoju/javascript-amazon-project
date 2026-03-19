350ece90b29debc1c35ee4722e07b0545108aa46
Oluwasanmisola Towoju <oluwasanmisolatowoju@gmail.com>
2026-03-19 19:43:01 +0100

    Implemented loadCart function to fetch cart data from backend and updated checkout process to load products and cart concurrently.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,23 @@
 import {renderOrderSummary} from './checkout/order-summary.js';
 import {renderPaymentSummary} from './checkout/payment-summary.js';
 import {loadProducts} from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
-});
+})
